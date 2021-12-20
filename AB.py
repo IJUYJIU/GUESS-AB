8c5b15df780d3956e454bca7b37e7216dffea1a1
IJUYJIU <95621220+IJUYJIU@users.noreply.github.com>
2021-12-20 20:10:24 +0800

    Update AB.py

--- AB.py
+++ AB.py
@@ -9,7 +9,7 @@ t = time.time()
 while a!=4:
   num += 1         
   a = b = n = 0
-  user = list(st.number_input('輸入四個數字'))
+  user = st.number_input('輸入四個數字')
 confirm_input = st.button("ok")
 if confirm_input:
   for i in user:

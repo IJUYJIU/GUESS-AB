15ed0e88cf1b818a4eceb0a9650319725224e378
IJUYJIU <95621220+IJUYJIU@users.noreply.github.com>
2021-12-20 19:50:05 +0800

    Update AB.py

--- AB.py
+++ AB.py
@@ -1,11 +1,13 @@
+import streamlit as st
+
 import random
-import time        # import time 模組
+import time       
 answer = random.sample(range(1, 10), 4)
 a = b = n = 0
-num = 0            # 新增 num 變數為 0，作為計算次數使用
-t = time.time()    # 新增 t 變數為現在的時間
+num = 0            
+t = time.time()    
 while a!=4:
-  num += 1         # 每次重複時將 num 增加 1
+  num += 1         
   a = b = n = 0
   user = list(input('輸入四個數字：'))
   for i in user:
@@ -16,7 +18,7 @@ while a!=4:
         b += 1
     n += 1
   output = ','.join(user).replace(',','')
-  print(f'{output}: {a}A{b}B')
-t = round((time.time() - t), 3)   # 當 a 等於 4 時，計算結束和開始的時間差
-print(f'答對了！總共猜了 {num} 次，用了 {t} 秒')   # 印出對應的文字
-print(answer)
+  st.write(f'{output}: {a}A{b}B')
+t = round((time.time() - t), 3)   
+st.write(f'答對了！總共猜了 {num} 次，用了 {t} 秒')   
+st.write(answer)

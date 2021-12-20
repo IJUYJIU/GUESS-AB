a1a49fd23b4a1c9a193e54c76a2f1fe05ced71e6
IJUYJIU <95621220+IJUYJIU@users.noreply.github.com>
2021-12-20 20:26:39 +0800

    Update AB.py

--- AB.py
+++ AB.py
@@ -2,25 +2,25 @@ import streamlit as st
 
 import random
 import time       
-answer = random.sample(range(1, 10), 4)
+w = random.sample(range(1, 10), 4)
 a = b = n = 0
 num = 0            
 t = time.time()    
 while a!=4:
   num += 1         
   a = b = n = 0
-  user = list(st.number_input('輸入四個數字'))
+  h = list(st.number_input('輸入四個數字'))
 confirm_input = st.button("ok")
 if confirm_input:
     for i in user:
-      if int(user[n]) == answer[n]:
+      if int(user[n]) == w[n]:
         a += 1
       else:
-        if int(i) in answer:
+        if int(i) in w:
           b += 1
       n += 1
-    st.number_output = ','.join(user).replace(',','')
+    st.number_output = ','.join(h).replace(',','')
     st.write(f'{output}: {a}A{b}B')
 t = round((time.time() - t), 3)   
 st.write(f'答對了！總共猜了 {num} 次，用了 {t} 秒')   
-st.write(answer)
+st.write(w)

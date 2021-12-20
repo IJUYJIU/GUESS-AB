05713809b2d1d6236956e732307f2ca92fae8d2c
IJUYJIU <95621220+IJUYJIU@users.noreply.github.com>
2021-12-20 20:20:44 +0800

    Update AB.py

--- AB.py
+++ AB.py
@@ -11,14 +11,14 @@ while a!=4:
   a = b = n = 0
   user = list(st.number_input('輸入四個數字'))
 confirm_input = st.button("ok")
-if confirm_input:
-  for i in user:
-    if int(user[n]) == answer[n]:
-      a += 1
-    else:
-      if int(i) in answer:
-        b += 1
-    n += 1
+  if confirm_input:
+    for i in user:
+      if int(user[n]) == answer[n]:
+        a += 1
+      else:
+        if int(i) in answer:
+          b += 1
+      n += 1
   st.number_output = ','.join(user).replace(',','')
   st.write(f'{output}: {a}A{b}B')
 t = round((time.time() - t), 3)   

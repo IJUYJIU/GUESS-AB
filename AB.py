3c01787d70e9beac921c54038e3b7801a692331d
IJUYJIU <95621220+IJUYJIU@users.noreply.github.com>
2021-12-20 20:48:24 +0800

    Update AB.py

--- AB.py
+++ AB.py
@@ -9,7 +9,7 @@ t = time.time()
 while a!=4:
   num += 1         
   a = b = n = 0
-  h = list(st.number_input('輸入四個數字'))
+  h = list(st.text_input('輸入四個數字', ''))
 confirm_input = st.button("ok")
 if confirm_input:
     for i in user:
@@ -19,8 +19,8 @@ if confirm_input:
         if int(i) in w:
           b += 1
       n += 1
-    st.number_output = ','.join(h).replace(',','')
-    st.write(f'{st.number_output}: {a}A{b}B')
+    output = ','.join(h).replace(',','')
+    st.write(f'{output}: {a}A{b}B')
 t = round((time.time() - t), 3)   
 st.write(f'答對了！總共猜了 {num} 次，用了 {t} 秒')   
 st.write(w)

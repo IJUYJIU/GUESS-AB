0749c4e9fbbcf15c1f07608c25d3e158a90b374e
IJUYJIU <95621220+IJUYJIU@users.noreply.github.com>
2021-12-27 20:02:40 +0800

    Update AB.py

--- AB.py
+++ AB.py
@@ -10,12 +10,16 @@ a = b = n = 0
 num = 0            # 新增 num 變數為 0，作為計算次數使用
 t = time.time()    # 新增 t 變數為現在的時間
 
+
+r = st.text_input('輸入四個數字：', default_value_goes_here)
+user = list(r)
+confirm_input = st.button("ok")
 default_value_goes_here = '1234'
 while a!=4:
   num += 1         # 每次重複時將 num 增加 1
   a = b = n = 0
-  r = st.text_input('輸入四個數字：', default_value_goes_here)
-  user = list(r)
+#  r = st.text_input('輸入四個數字：', default_value_goes_here)
+#  user = list(r)
   confirm_input = st.button("ok")
   if confirm_input:
     for i in user:

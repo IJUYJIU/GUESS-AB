651381953e614f63c4d17c5a8e4929313611a2d5
IJUYJIU <95621220+IJUYJIU@users.noreply.github.com>
2022-01-03 19:05:20 +0800

    Update AB.py

--- AB.py
+++ AB.py
@@ -1,38 +1,48 @@
-import streamlit as st
-#default_value_goes_here = '1234'
-#user_input = st.text_input("label goes here", default_value_goes_here)
-
 import random
-import time        # import time 模組
-answer = random.sample(range(1, 10), 4)
-#print(answer)
-st.write(answer)
-a = b = n = 0
-num = 0            # 新增 num 變數為 0，作為計算次數使用
-t = time.time()    # 新增 t 變數為現在的時間
+import streamlit as st
 
-default_value_goes_here = '1234'
-#r = st.text_input('輸入四個數字：', default_value_goes_here)
-#user = list(r)
-#confirm_input = st.button("ok")
-while a!=4:
-  num += 1         # 每次重複時將 num 增加 1
-  a = b = n = 0
-  r = st.text_input('輸入四個數字：', default_value_goes_here)
-  user = list(r)
-  confirm_input = st.button("ok")
-  if confirm_input:
-    confirm_input=0
-    for i in user:
-       if int(user[n]) == answer[n]:
-        a += 1
-       else:
-        if int(i) in answer:
-         b += 1
-       n += 1
-  output = ','.join(user).replace(',','')
-  #print(f'{output}: {a}A{b}B')
-  st.write('output:',a,'A', b, 'B')
-#if confirm_input:
-#  t = round((time.time() - t), 3)   # 當 a 等於 4 時，計算結束和開始的時間差
-#  print(f'答對了！總共猜了 {num} 次，用了 {t} 秒')   # 印出對應的文字
+st.title('1A2B game')
+st.markdown("""
+大家應該都有玩過這個猜數字的遊戲吧，
+A 代表的是：數字猜對位子也對。
+B 代表的是：數字對了，但是位子不對。
+0～9會隨機抽出不重複的四位數字，準備好就開始吧 
+""")
+st.title('👇👇')
+
+items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+random.shuffle(items)
+#st.write(answer)
+
+answer=''
+
+a_count=0 # initial A count
+b_count=0 # initial B count
+
+submit_button = st.button(label='開始遊戲')   
+if submit_button:
+    for i in range(4):
+        answer+=str(items[i])
+    st.session_state.answer=answer
+
+number=st.sidebar.text_input('請輸入數字')
+#while(True):
+    #number=st.text_input('Enter the number: ')
+#st.write(st.session_state.answer)
+if not number.isdigit():  #cheak all input is digit
+    pass
+else:
+    if number==st.session_state.answer:
+        st.write('好棒棒！你猜對了')
+        #break
+    for i in range(4):
+        #st.session_state(st.session_state.answer)
+        for j in range(4):
+            if i==j and number[i]==st.session_state.answer[j]:
+                a_count+=1
+            elif number[i]==st.session_state.answer[j]:
+                b_count+=1
+            #    st.session_state(answer)
+    st.write(a_count, 'A', b_count, 'B')
+    a_count=0
+    b_count=0
